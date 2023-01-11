5e55ee42474752164b3471f75d024435e4b05a62
kevin matthew T <91600300+kevinMatthewT@users.noreply.github.com>
2023-01-11 23:10:13 +0700

    Create insertData.sql

---
+++ insertData.sql
@@ -0,0 +1,34 @@
+insert into pla ne(Plane_ID,Plane_Type)
+values
+  (1,"Tiger Air"),
+  (2,"Sky High Airlines");
+
+insert into flight(Flight_Number,Plane_ID,Departure_time,Departure_city,Departure_country,Arrival_time,Arrival_city,Arrival_country)
+values
+    (1,1,"2023-01-06 15:00:00","Seoul","South Korea","2023-01-06 17:10:00","Osaka","Japan"),
+    (2,2,"2023-01-06 23:00:00","Jakarta","Indonesia","2023-01-07 02:00:00","Tokyo","Japan");
+   
+   
+insert into passenger(Passenger_ID,Name,Age,Sex,Date_of_birth,Address,Phone_Number,Email)
+values
+    (1,"Dainton Parker",30,"M","1993-01-02","Jl. Danau Sunter Utara No.45, Sunter Agung, Kec. T...",656083944,"d.parker@randatmail.com"),
+    (2,"Adrianna Walker",23,"F","2000-01-02","208-208 上岩田 小国町 Nagaoka, Niigata 949-5332, Japan",930707387,"a.walker@randatmail.com"),
+    (3,"Preston Armstrong",29,"M","1994-01-02","95XJ+43M, Yingbin Blvd, Huadu District, Guangzhou,...",371009041,"p.armstrong@randatmail.com"),
+    (4,"Richard Perry",26,"M","1997-01-02","60 Airport Boulevard #048-049, Terminal 2 Singapore",355261138,"r.perry@randatmail.com"),
+    (5,"Amy Ellis",25,"F","1998-01-02","3 Stadium Walk, Singapura 397692",142266820,"a.ellis@randatmail.com"),
+    (6,"Maximillian Thompson",18,"M","2005-01-02","Harare, Zimbabwe",11969896,"m.thompson@randatmail.com"),
+    (7,"Naomi Murray",25,"F","1998-01-02","64 Big Rock Cove St. Pueblo, CO 81001",695624873,"n.murray@randatmail.com"),
+    (8,"Walter Allen",28,"M","2001-01-02","8 Armstrong Street Olive Branch, MS 38654",138564715,"w.allen@randatmail.com"),
+    (9,"Julia Carter",21,"F","2008-01-02","41 North Overlook Street Seymour, IN 47274",426987844,"j.carter@randatmail.com"),
+    (10,"Tony Nelson",18,"M","2005-01-02","89 Shady Road Beverly, MA 01915",43381070,"t.nelson@randatmail.com"),
+    (11,"Lucia Baker",30,"F","1993-01-02","81 Golden Star St. Irwin, PA 15642",810340478 ,"l.baker@randatmail.com"),
+    (12,"Maya Dixon",36,"F","1987-01-02","110 York Street Rock Hill, SC 29730",111192037,"m.dixon@randatmail.com"),
+    (13,"Lydia Tucker",33,"F","1990-01-02","4585 Shimerville Rd Clarence, New York(NY), 14031",926400561,"l.tucker@randatmail.com"),
+    (14,"Preston Anderson",40,"M","1983-01-02","(870) 535-6166 2913 S Mississippi St Pine Bluff,...",534981197,"p.anderson@randatmail.com"),
+    (15,"Ashton Hamilton",40,"M","1983-01-02","105 Harmony Path Bonaire, Georgia(GA), 31005",811509560,"a.hamilton@randatmail.com"),
+    (16,"Chester Crawford",3,"M","1984-01-02","2314 Duckett St #APT C Conway, South Carolina(SC)..",419153453,"c.crawford@randatmail.com"),
+    (17,"Lyndon Richards",40,"M","1983-01-02","20037 30th Hwy Buhl, Idaho(ID), 83316",379855584,"l.richards@randatmail.com"),
+    (18,"Jack Reed",31,"M","1992-01-02","218 S Pine St Batesburg, South Carolina(SC), 2900...",643393572,"j.reed@randatmail.com"),
+    (19,"William Farell",32,"M","1991-01-02","1472 Wesley Dr Griffin, Georgia(GA), 30224",556333480,"w.farrell@randatmail.com"),
+    (20,"Aline Murphy",31,"F","1992-01-02","16544 NE North Main St Summitville, Indiana(IN), ...",835975603,"a.murphy@randatmail.com")
+    ;
